9d8b1bccf0a976df41e170fc6bdf675e1ee081f0
Pratik <167973139+prateekraiger@users.noreply.github.com>
2024-04-30 15:11:28 +0530

    Time: 1 ms (100%), Space: 46.2 MB (30.63%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,25 +1,45 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        //using Queue
+        // if(nums.length==1){
+        //     return;
+        // }
+
+        // Queue<Integer> q=new LinkedList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]!=0){
+        //         q.add(nums[i]);
+        //     }
+        // }
+
+        // int idx=0;
+        // while(q.size() !=0){
+        //     nums[idx]=q.poll();
+        //     idx++;
+        // }
+
+        // for(int i=idx;i<nums.length;i++){
+        //     nums[i]=0;
+        // }
+
+
+        // using 2 pointers
         if(nums.length==1){
             return;
         }
 
-        Queue<Integer> q=new LinkedList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                q.add(nums[i]);
+        int left=0;
+        for(int right=0;right<nums.length;right++){
+            if(nums[right]!=0){
+                nums[left]=nums[right];
+                left++;
             }
         }
 
-        int idx=0;
-        while(q.size() !=0){
-            nums[idx]=q.poll();
-            idx++;
-        }
-
-        for(int i=idx;i<nums.length;i++){
+        for(int i=left;i<nums.length;i++){
             nums[i]=0;
         }
+
 
     }
 }
