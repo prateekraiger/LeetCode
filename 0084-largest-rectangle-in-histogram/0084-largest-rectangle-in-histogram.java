2e47948d3459be38ef36f17c3604e168101f902f
Pratik <167973139+prateekraiger@users.noreply.github.com>
2024-05-07 15:59:44 +0530

    Time: 66 ms (60.79%), Space: 57.1 MB (67.04%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,51 +1,39 @@
 class Solution {
-    public int largestRectangleArea(int[] arr) {
-        
+    public static int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int area = 0;
+        int i;
 
-        int maxArea=0;
-        int nsr[]=new int [arr.length];
-        int nsl[]=new int [arr.length];
-
-        //next smaller right
-        Stack<Integer> stk=new Stack<>();
-        for(int i=arr.length-1;i>=0;i--){
-            while(!stk.isEmpty() && arr[stk.peek()] >= arr[i] ){
-                stk.pop();
+        for (i = 0; i < heights.length;) {
+            if (stack.isEmpty() || heights[stack.peek()] <= heights[i]) {
+                stack.push(i++);
+            } else {
+                int top = stack.pop();
+                if (stack.isEmpty()) {
+                    area = heights[top] * i;
+                } else {
+                    area = heights[top] * (i - stack.peek() - 1);
+                }
+                if (area > maxArea) {
+                    maxArea = area;
+                }
             }
-            if(stk.isEmpty()){
-                nsr[i]=arr.length;
-            }
-            else{
-                nsr[i]=stk.peek();
-            }
-            stk.push(i);
-        }
-        //next smaller left
-        stk=new Stack<>();
-        for(int i=0;i<arr.length;i++){
-            while(!stk.isEmpty() && arr[stk.peek()] >= arr[i] ){
-                stk.pop();
-            }
-            if(stk.isEmpty()){
-                nsl[i]=-1;
-            }
-            else{
-                nsl[i]=stk.peek();
-            }
-            stk.push(i);
         }
 
-        //current area=widtth=j-i-1= nsr[i]- nsl[i] -1
-        for(int i=0;i<arr.length;i++){
-            int hei=arr[i];
-            int wid=nsr[i] -nsl[i] -1;
-            int currArea=hei*wid;
-
-            maxArea=Math.max(maxArea, currArea);
+        while (!stack.isEmpty()) {
+            int top = stack.pop();
+            if (stack.isEmpty()) {
+                area = heights[top] * i;
+            } else {
+                area = heights[top] * (i - stack.peek() - 1);
+            }
+            if (area > maxArea) {
+                maxArea = area;
+            }
         }
 
-        return maxArea;
-        
+        return maxArea;       
     
     }
 }
